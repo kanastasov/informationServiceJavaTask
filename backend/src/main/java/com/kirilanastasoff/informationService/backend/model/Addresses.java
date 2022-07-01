914eec757c727c0906ee5b2c6c3f240e9f35db26
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-07-01 08:51:13 +0200

    added the model and db data

---
+++ backend/src/main/java/com/kirilanastasoff/informationService/backend/model/Addresses.java
@@ -0,0 +1,35 @@
+package com.kirilanastasoff.informationService.backend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+@Table(name = "addresses")
+public class Addresses {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@JsonBackReference
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "people_id", referencedColumnName = "id")
+	private People people;
+
+	@NotNull
+	@Column(name = "addr_type", length = 5)
+	private String addrType;
+
+	@Column(name = "addr_info", length = 300)
+	private String addrInfo;
+
+}
