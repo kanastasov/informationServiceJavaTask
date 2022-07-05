946e4963ea2d0f4a0b63893667e498b2abc10bed
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-07-05 08:08:10 +0200

    add the search functionallity for table by fullName

--- backend/src/main/java/com/kirilanastasoff/informationService/backend/repository/MailsRepository.java
+++ backend/src/main/java/com/kirilanastasoff/informationService/backend/repository/MailsRepository.java
@@ -1,9 +1,16 @@
 package com.kirilanastasoff.informationService.backend.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kirilanastasoff.informationService.backend.model.Addresses;
 import com.kirilanastasoff.informationService.backend.model.Mails;
+import com.kirilanastasoff.informationService.backend.model.People;
 
 public interface MailsRepository extends JpaRepository<Mails, Long> {
+	
+	Optional<Mails> findByEmail(String email);
+
 }
